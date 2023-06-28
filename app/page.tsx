85f79d471d58029d050dd3a76d006cf1bffa83a0
Esteban Jimenez <estebandjs7@gmail.com>
2023-06-28 17:19:46 -0400

    Add component Hero

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Payment from "@/components/Payment";
 import { InputCoso } from "@/components/Input";
 import { ButtonDemo } from "@/components/Button";
 import { delay, motion } from "framer-motion";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className=" flex justify-center items-center h-screen bg-background">
+    <main className=" grid 	place-items-center h-screen bg-background">
+      <Hero/>
       <Payment/>
      
       
