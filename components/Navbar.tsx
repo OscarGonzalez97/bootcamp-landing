188aa311732fcf9d730c54bf2b8433b7db81e69d
Franco Pecci <fapeccisan@gmail.com>
2023-06-29 09:21:49 -0400

    Diseño hecho

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Nav (){
     <div className='shadow-md w-full fixed top-0 left-0'>
       <div className='md:flex h-32 justify-between bg-secondary-foreground py-4 md:px-10 px-7'>
         <div className='text-2xl cursor-pointer lg:flex items-center text-white font-[Arial] text-gray-800'>
-          <img src='https://www.roshka.com/static/img/all-white-logo.svg' alt='' onClick={handleToggleMobileOptions}/>
+          <img src='https://www.roshka.com/static/img/all-white-logo.svg' onClick={handleToggleMobileOptions}/>
         </div>
         <div className={`hidden md:contents ${showMobileOptions ? '' : 'hidden-mobile'}`}>
           <div className='text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] text-gray-800'>
@@ -23,10 +23,10 @@ export default function Nav (){
         </div>
         {showMobileOptions && (
           <div className='md:hidden'>
-            <div className='mt-4 text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] text-gray-800'>
+            <div className='mt-7 text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] text-gray-800'>
               Bootcamps
             </div>
-            <div className='mt-4 text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] text-gray-800'>
+            <div className='mt-7 text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] text-gray-800'>
               Bootcamps Realizados
             </div>
           </div>
