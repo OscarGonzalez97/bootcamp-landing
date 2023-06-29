6167f2d04ebcf12e4b9f4bc5755cbc05692576f8
Franco Pecci <fapeccisan@gmail.com>
2023-06-29 08:54:48 -0400

    Mobile añadido, falta diseño

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,35 @@
 import React, { useState } from 'react';
 
 export default function Nav (){
-  const [showOptions, setShowOptions] = useState(false);
+  const [showMobileOptions, setShowMobileOptions] = useState(false);
 
-  const handleToggleOptions = () => {
-    setShowOptions(!showOptions);
+  const handleToggleMobileOptions = () => {
+    setShowMobileOptions(!showMobileOptions);
   };
 
   return (
     <div className='shadow-md w-full fixed top-0 left-0'>
       <div className='md:flex h-32 justify-between bg-secondary-foreground py-4 md:px-10 px-7'>
         <div className='text-2xl cursor-pointer lg:flex items-center text-white font-[Arial] text-gray-800'>
-          <img src='https://www.roshka.com/static/img/all-white-logo.svg' alt='' onClick={handleToggleOptions}/>
+          <img src='https://www.roshka.com/static/img/all-white-logo.svg' alt='' onClick={handleToggleMobileOptions}/>
         </div>
-        {showOptions && (
-          <React.Fragment>
-            <div className='text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] text-gray-800'>
+        <div className={`hidden md:contents ${showMobileOptions ? '' : 'hidden-mobile'}`}>
+          <div className='text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] text-gray-800'>
+            Bootcamps
+          </div>
+          <div className='text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] text-gray-800'>
+            Bootcamps Realizados
+          </div>
+        </div>
+        {showMobileOptions && (
+          <div className='md:hidden'>
+            <div className='mt-4 text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] text-gray-800'>
               Bootcamps
             </div>
-            <div className='text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] text-gray-800'>
+            <div className='mt-4 text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] text-gray-800'>
               Bootcamps Realizados
             </div>
-          </React.Fragment>
+          </div>
         )}
       </div>
     </div>
