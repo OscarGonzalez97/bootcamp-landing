e2e6942af42844ccc7f8e4e5162614d409e3e0d5
Franco Pecci <fapeccisan@gmail.com>
2023-06-29 10:54:41 -0400

    Avances..

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,12 +11,17 @@ export default function Nav (){
     <div className='w-full fixed top-0 left-0'>
       <div className='md:flex h-32 justify-between bg-secondary-foreground py-7 md:px-10 px-7'>
         <div className='text-2xl cursor-pointer lg:flex items-center text-white font-[Arial]'>
-          <img src='https://www.roshka.com/static/img/all-white-logo.svg' className='w-72' onClick={handleToggleMobileOptions}/>
+          <img src='https://www.roshka.com/static/img/all-white-logo.svg' className='w-72'/>
         </div>
         <div className={`hidden md:contents ${showMobileOptions ? '' : 'hidden-mobile'}`}>
           <div className='text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial]'>
             Bootcamps Realizados
           </div>
+        </div>
+        <div className={`md:hidden ${showMobileOptions ? '' : ''}`}>
+          <button className='text-2xl hover:text-slate-400 cursor-pointer flex items-center text-white font-[Arial] mt-14' onClick={handleToggleMobileOptions}>
+            {showMobileOptions ? "☰" : "☰"}
+          </button>
         </div>
         {showMobileOptions && (
           <div className='md:hidden'>
