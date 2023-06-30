b15ace6cc27a19a21df59e5fe06e1070238e0356
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-06-30 15:39:24 -0400

    enviroment correcto

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,12 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-06-30'
 
 export const dataset = assertValue(
-  process.env.SANITY_STUDIO_DATASET,
+  process.env.SANITY_STUDIO_DATASET || "production",
   'Missing environment variable: SANITY_STUDIO_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.SANITY_STUDIO_PROJECT_ID,
+  process.env.SANITY_STUDIO_PROJECT_ID || "2ing4kvl",
   'Missing environment variable: SANITY_STUDIO_PROJECT_ID'
 )
 
