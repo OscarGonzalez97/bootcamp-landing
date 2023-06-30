6ef519d3109f4c71ddfafca8800da9437083362d
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-06-30 12:20:25 -0400

    add: se crea schema de curso

---
+++ sanity/schemas/curso.ts
@@ -0,0 +1,54 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'curso',
+  title: 'Curso',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'titulo',
+      title: 'Titulo',
+      type: 'string',
+    }),
+    defineField({
+        name: 'descripcion',
+        title: 'Descripcion',
+        type: 'text',
+      }),
+    defineField({
+      name: 'slug',
+      title: 'Slug/URL',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+        name: 'imagen',
+        title: 'Imagen',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+    }),
+    defineField({
+      name: 'modulos',
+      title: 'Modulos',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'modulo'}}],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+  ],
+
+  preview: {
+    select: {
+      titulo: 'titulo',
+      descripcion: 'descripcion',
+    },
+  },
+})
