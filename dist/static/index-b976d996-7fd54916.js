985cde257b4810d986fd1796de1ece161e609667
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-06-30 12:21:07 -0400

    add: se agrega directorio que luego es deployado a la nube de sanity

---
+++ dist/static/index-b976d996-7fd54916.js
@@ -0,0 +1,1 @@
+import{DocumentPane as t}from"./index-0de920bc-510a9562.js";import{D as m}from"./sanity-c71bb8a0.js";import"./json-inspector-30ad798b.js";export{m as DocumentPaneProvider,t as default};
