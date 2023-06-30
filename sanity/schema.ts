58909effe62045618b3dd94f819af07d55cc211e
MedinaEnrique <133240252+MedinaEnrique@users.noreply.github.com>
2023-06-30 16:23:30 -0400

    add : esquema bootcamp realizado

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import curso from './schemas/curso'
 import modulo from './schemas/modulo'
+import bootcampsRealizados from './schemas/bootcampsRealizados'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [curso, modulo],
+  types: [curso, modulo,bootcampsRealizados],
 }
---
+++ sanity/schemas/bootcampsRealizados.ts
@@ -0,0 +1,33 @@
+
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+    name: 'bootcampRealizado',
+    title: 'Bootcamp Realizado',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'fechaDesde',
+            title: 'Fecha Desde',
+            type: 'date',
+        }),
+        defineField({
+            name: 'fechaHasta',
+            title: 'Fecha Hasta',
+            type: 'date',
+        }),
+        defineField({
+            name: 'curso',
+            title: 'Curso',
+            type: 'curso',
+        }),
+        defineField({
+            name: 'imagen',
+            title: 'Imagen',
+            type: 'image',
+            options:{
+                hotspot: true
+            }
+        }),
+    ]
+})
