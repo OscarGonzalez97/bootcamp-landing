0291ab4979bf4e9a11a05df1ad33aa49a283a7fe
MedinaZS <101607623+MedinaZS@users.noreply.github.com>
2023-06-30 16:10:58 -0400

    Update: Cambie el acordion por un componente

--- app/bootcamps-realizados/[slug]/page.tsx
+++ app/bootcamps-realizados/[slug]/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import Image from 'next/image'
@@ -9,6 +10,8 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion"
+import { AccordionDemo } from '@/components/Accordion'
+import { IBootcampRealizado } from '@/components/types'
 
 interface parametros {
     params: {
@@ -16,17 +19,41 @@ interface parametros {
     }
 }
 
-function page({ params }: parametros) {
+const Page = ({ params }: parametros) => {
 
     console.log(params.slug)
 
     const bootcampDetail = {
-        bootcamp: { title: "Bootcamp React", description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusantium maxime ex libero ratione distinctio minus excepturi error incidunt facere, a dolore, vitae, vero reprehenderit perferendis culpa sunt dolores odio.', date_from: '12/04/2023', date_to: '07/07/2023', image: 'https://images.unsplash.com/photo-1674574124649-778f9afc0e9c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80' },
+        title: "Bootcamp React",
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusantium maxime ex libero ratione distinctio minus excepturi error incidunt facere, a dolore, vitae, vero reprehenderit perferendis culpa sunt dolores odio.',
+        date_from: '12/04/2023',
+        date_to: '07/07/2023',
+        image: 'https://images.unsplash.com/photo-1674574124649-778f9afc0e9c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
         projects: [
-            { title: "Proyecto 1", description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusantium maxime ex libero ratione distinctio minus excepturi error incidunt facere, a dolore, vitae, vero reprehenderit perferendis culpa sunt dolores odio.', image: 'https://images.unsplash.com/photo-1559028006-448665bd7c7f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80' },
-            { title: "Proyecto 2", description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusantium maxime ex libero ratione distinctio minus excepturi error incidunt facere, a dolore, vitae, vero reprehenderit perferendis culpa sunt dolores odio.', image: 'https://images.unsplash.com/photo-1573495611745-41a6963351ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80' },
+            {
+                title: "Proyecto 1",
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusantium maxime ex libero ratione distinctio minus excepturi error incidunt facere, a dolore, vitae, vero reprehenderit perferendis culpa sunt dolores odio.',
+                image: [
+                    'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80',
+                    'https://images.unsplash.com/photo-1573495611745-41a6963351ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80',
+                    'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80',
+                    'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80',
+                ]
+            },
+            {
+                title: "Proyecto 2",
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusantium maxime ex libero ratione distinctio minus excepturi error incidunt facere, a dolore, vitae, vero reprehenderit perferendis culpa sunt dolores odio.',
+                image: [
+                    'https://images.unsplash.com/photo-1573495611745-41a6963351ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80',
+                    'https://images.unsplash.com/photo-1573495611745-41a6963351ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80',
+                    'https://images.unsplash.com/photo-1573495611745-41a6963351ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80',
+                ]
+            },
         ]
+
     }
+
+    const [bootcamp, setbootcamp] = useState<IBootcampRealizado>(bootcampDetail)
 
     return (
         <div className='container grid gap-10 mt-4'>
@@ -36,19 +63,19 @@ function page({ params }: parametros) {
                 {/* Detalle bootcamp */}
                 <div className='text-foreground grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-20 '>
                     <div className=''>
-                        <Image className='object-cover w-full h-auto object-top rounded-xl roun' src={bootcampDetail.bootcamp.image} alt={bootcampDetail.bootcamp.title} width={1000} height={1000}></Image>
+                        <Image className='object-cover w-full h-auto object-top rounded-xl' src={bootcamp.image} alt={bootcamp.title} width={1000} height={1000}></Image>
                     </div>
                     <div>
-                        <h2 className="text-2xl sm:text-5xl font-bold text-foreground ">{bootcampDetail.bootcamp.title}</h2>
+                        <h2 className="text-2xl sm:text-5xl font-bold text-foreground ">{bootcamp.title}</h2>
                         <hr className='my-5 border-accent' />
                         <p className='flex text-xl mb-10 items-center'>
                             <CalendarDays className='text-accent me-2' />
                             <span>
-                                Desde:  {bootcampDetail.bootcamp.date_from} hasta {bootcampDetail.bootcamp.date_to}
+                                Desde:  {bootcamp.date_from} hasta {bootcamp.date_to}
                             </span>
                         </p>
 
-                        <p className='text-xl'>{bootcampDetail.bootcamp.description} </p>
+                        <p className='text-xl'>{bootcamp.description} </p>
 
                     </div>
                 </div>
@@ -63,23 +90,7 @@ function page({ params }: parametros) {
                             </div>
 
                             <div>
-                                <Accordion type="single" collapsible  className='grid gap-7'>
-                                    {bootcampDetail.projects.map((item, index) => (
-                                        <AccordionItem value={'item-' + index} key={index} className='bg-muted hover:bg-secondary-foreground shadow-md text-muted-foreground rounded-xl border-b-0'>
-                                            <AccordionTrigger className='rounded-xl text-xl sm:text-2xl  hover:no-underline px-5'>
-                                                {item.title}
-                                            </AccordionTrigger>
-                                            <AccordionContent className='bg-muted-dark border-muted border-2 border-t-0 rounded-b-xl'>
-                                                <div className='p-5 '>
-                                                    <div className='grid grid-cols-1 md:grid-cols-3 gap-y-5 md:gap-5'>
-                                                        <Image className='w-full h-auto' src={item.image} alt={item.title} width={300} height={100}></Image>
-                                                        <p className='text-foreground text-xl col-span-2'>{item.description}</p>
-                                                    </div>
-                                                </div>
-                                            </AccordionContent>
-                                        </AccordionItem>
-                                    ))}
-                                </Accordion>
+                                <AccordionDemo projects={bootcampDetail.projects}></AccordionDemo>
                             </div>
                         </div>
                     </div>
@@ -89,4 +100,4 @@ function page({ params }: parametros) {
     )
 }
 
-export default page
+export default Page
--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -1,37 +1,38 @@
+import { IProjects } from "./types"
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
 } from "./ui/accordion"
+
 import Image from "next/image"
 
-const bootcampDetail = {
-  bootcamp: { title: "Bootcamp React", description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusantium maxime ex libero ratione distinctio minus excepturi error incidunt facere, a dolore, vitae, vero reprehenderit perferendis culpa sunt dolores odio.', date_from: '12/04/2023', date_to: '07/07/2023', image: 'https://images.unsplash.com/photo-1674574124649-778f9afc0e9c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80' },
-  projects: [
-      { title: "Proyecto 1", description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusantium maxime ex libero ratione distinctio minus excepturi error incidunt facere, a dolore, vitae, vero reprehenderit perferendis culpa sunt dolores odio.', image: 'https://images.unsplash.com/photo-1559028006-448665bd7c7f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80' },
-      { title: "Proyecto 2", description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusantium maxime ex libero ratione distinctio minus excepturi error incidunt facere, a dolore, vitae, vero reprehenderit perferendis culpa sunt dolores odio.', image: 'https://images.unsplash.com/photo-1573495611745-41a6963351ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80' },
-  ]
-}
-
-export function AccordionDemo() {
-  return (
-    <Accordion type="single" collapsible  className='grid gap-7'>
-    {bootcampDetail.projects.map((item, index) => (
-        <AccordionItem value={'item-' + index} key={index} className='bg-muted hover:bg-secondary-foreground shadow-md text-muted-foreground rounded-xl border-b-0'>
-            <AccordionTrigger className='rounded-xl text-xl sm:text-2xl  hover:no-underline px-5'>
-                {item.title}
-            </AccordionTrigger>
-            <AccordionContent className='bg-muted-dark border-muted border-2 border-t-0 rounded-b-xl'>
-                <div className='p-5 '>
-                    <div className='grid grid-cols-1 md:grid-cols-3 gap-y-5 md:gap-5'>
-                        <Image className='w-full h-auto' src={item.image} alt={item.title} width={300} height={100}></Image>
-                        <p className='text-foreground text-xl col-span-2'>{item.description}</p>
-                    </div>
-                </div>
-            </AccordionContent>
-        </AccordionItem>
-    ))}
-</Accordion>
-  )
+export function AccordionDemo({ projects }: { projects: Array<IProjects> }) {
+	// console.log(projects)
+	return (
+		<Accordion type="single" collapsible className='grid gap-7' defaultValue='item-0'>
+			{projects && projects.map((item, index) => (
+				<AccordionItem value={'item-' + index} key={index} className='bg-muted hover:bg-secondary-foreground shadow-md text-muted-foreground rounded-xl border-b-0'>
+					<AccordionTrigger className='rounded-xl text-xl sm:text-2xl  hover:no-underline px-5'>
+						{item.title}
+					</AccordionTrigger>
+					<AccordionContent className='bg-muted-dark border-muted border-2 border-t-0 rounded-b-xl'>
+						<div className='grid gap-5 p-5 text-xl'>
+							{/* Descripcion */}
+							{/* <p className="font-bold underline underline-offset-8 decoration-accent">Descripción:</p> */}
+							<p className='text-foreground text-lg text-gray-300'>{item.description}</p>
+							{/* Imagenes */}
+							<p className="font-bold underline underline-offset-8 decoration-accent tracking-wider">Galeria de imagenes:</p>
+							<div className='flex flex-wrap gap-5'>
+								{item.image.map((imagen, index) => (
+									<Image key={index} className='w-auto h-min md:h-80' src={imagen} alt={item.title} width={300} height={100}></Image>
+								))}
+							</div>
+						</div>
+					</AccordionContent>
+				</AccordionItem>
+			))}
+		</Accordion>
+	)
 }
