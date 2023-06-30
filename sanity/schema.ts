02ef9469997362379ea0fe32ef5981afcd6b5ef0
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-06-30 16:41:19 -0400

    fix: se agrega contenido a la definicion de schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -3,7 +3,8 @@ import curso from './schemas/curso'
 import modulo from './schemas/modulo'
 import bootcampsRealizados from './schemas/bootcampsRealizados'
 import proyecto from './schemas/proyecto'
+import contenido from './schemas/contenido'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [curso, modulo, bootcampsRealizados, proyecto, ],
+  types: [curso, modulo, bootcampsRealizados, proyecto, contenido],
 }
