acff7e9d066e189ad81df9440b6835bee9f1fef7
Agustin Ramirez <agusramirez2001@gmail.com>
2023-07-03 12:02:59 -0400

    Update: Se corrigió las cards

--- app/page.tsx
+++ app/page.tsx
@@ -7,33 +7,14 @@ import Footer from "@/components/Footer";
 import Carousel from "@/components/Carousel";
 import Loader from "@/components/Loader";
 
-interface Curso {
-  _id: string;
-  titulo: string;
-  // add any other properties here
-}
-
 export async function generateStaticParams() {
-  return [<Loader />];
+  return [<Loader/>];
 }
 
 export default function Home() {
-  const [cursoData, setCursoData] = useState<Curso[] | null>(null);
   const [carouselData, setCarouselData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchCursoData = async () => {
-      try {
-        const data = await fetchAllCurso();
-        setCursoData(data?.allCurso || []);
-      } catch (error) {
-        console.error("Error fetching curso data:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
     const fetchCarouselData = async () => {
       try {
         const response = await fetch("https://rickandmortyapi.com/api/character");
@@ -44,7 +25,6 @@ export default function Home() {
       }
     };
 
-    fetchCursoData();
     fetchCarouselData();
   }, []);
 
@@ -54,17 +34,6 @@ export default function Home() {
     <main className="grid place-items-center h-screen bg-background">
       <Hero />
       <Cards />
-      {cursoData && (
-        <div className="curso-list">
-          {cursoData.map((curso) => (
-            <div key={curso._id}>{curso.titulo}</div>
-       
-
-
-          ))}
-        </div>
-      )}
-       {isLoading && <Loader />}
       {carouselData && <Carousel data={carouselData} />}
       <Footer />
     </main>
