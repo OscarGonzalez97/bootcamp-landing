7fefeb8480c6821167ede91790689e2d2209ec67
MedinaZS <101607623+MedinaZS@users.noreply.github.com>
2023-07-05 19:03:40 -0400

    Se elimina carpeta dist y se añade a gitignore

--- dist/static/index-b976d996-22190de4.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPane as t}from"./index-0de920bc-0c56c146.js";import{D as m}from"./sanity-13705189.js";import"./json-inspector-63cec2e3.js";export{m as DocumentPaneProvider,t as default};
