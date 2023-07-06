8ab623eadc3e281978d992c0bbc41c6be3fb6e70
MedinaZS <101607623+MedinaZS@users.noreply.github.com>
2023-07-05 20:26:43 -0400

    Se cambia el icono de cargando pagina

--- app/bootcamp/[slug]/page.tsx
+++ app/bootcamp/[slug]/page.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { AccordionBtc } from "@/components/AccordionBootcamp";
 import { fetchCursoBySlug } from "@/services/querys";
+import Loader from "@/components/Loader";
 
 interface Curso {
   _id: string;
@@ -27,34 +28,43 @@ interface Props {
 
 export default function Home({ params }: Props) {
   const [curso, setCurso] = useState<Curso | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
   const slug = params.slug;
 
   useEffect(() => {
     async function fetchData() {
       const cursoData = await fetchCursoBySlug(slug);
       setCurso(cursoData);
+      setIsLoading(false);
     }
 
     fetchData();
   }, [slug]);
 
-  if (!curso) {
-    return <div>Loading...</div>; // or any loading state you want to show
-  }
+  // if (!curso) {
+  //   return <div>Loading...</div>; // or any loading state you want to show
+  // }
 
   return (
     <main className="text-secondary justify-center bg-green-600 h-screen flex">
-      <div className="grid place-items-center gap-4 w-full  bg-background">
-        <div className=" w-9/12 grid gap-8">
-          <div className="flex flex-col items-start">
-            <h1 className="text-4xl font-bold text-center">{curso.titulo}</h1>
-            <p className="text-xl ">{curso.descripcion}</p>
-          </div>
-          <div className="w-full flex flex-col j gap-4 ustify-center items-center border-2 border-secondary-foreground rounded-lg p-4">
-            <h1 className="self-start text-xl font-bold">Modulos</h1>
-            <AccordionBtc modulos={curso.modulos} />
-          
-        </div>
+      <div className={"grid place-items-center gap-4 w-full  bg-background " + ((!isLoading) ? 'place-content-baseline' : '')}>
+        {isLoading && <div><Loader /></div>}
+        <div className=" w-9/12 grid gap-8 mt-5">
+
+          {/* Header */}
+          {curso &&
+            <>
+              <div className="grid gap-5">
+                <h1 className="text-2xl sm:text-5xl font-bold text-foreground text-center">{curso.titulo}</h1>
+                <hr className='border-accent' />
+                <p className="text-xl ">{curso.descripcion}</p>
+              </div>
+              <div className="w-full flex flex-col gap-4 justify-center items-center border-2 border-secondary-foreground rounded-lg p-4">
+                <h1 className="self-start text-xl font-bold">Modulos</h1>
+                <AccordionBtc modulos={curso.modulos} />
+              </div>
+            </>
+          }
         </div>
       </div>
     </main>
